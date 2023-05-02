ef74292ed5f3ef159c2ddd4f741d9f09fd30b4cf
Gustavo Takeda <gusmitsuo@gmail.com>
2023-05-02 17:01:25 -0300

    Basic Commit

---
+++ src/Base/App.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+//TODO:ler um .INI para configurar logo de cara
+class App
+{
+    public:
+        void Setup();
+        
+        void Input();
+        void Update();
+        void Draw();
+        void Run();
+        
+    private:
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <Base/App.hpp>
+
+int main()
+{
+    App app;
+    app.Run();
+    return 0;
+}
