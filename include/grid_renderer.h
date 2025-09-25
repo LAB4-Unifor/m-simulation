a6896fac7be240aadc484738d15295bda2706342
Alex <alexandrepinto586@gmail.com>
2025-09-25 16:45:35 -0300

    add the grid_renderer.h

---
+++ include/grid_renderer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include "renderer.h"
+
+class GridRenderer {
+private:
+    GLuint VAO, VBO;
+    bool enabled;
+    float gridSize;
+    int gridDivisions;
+
+public:
+    GridRenderer();
+    ~GridRenderer();
+    void initialize();
+    void render(const glm::mat4& view, const glm::mat4& project)
+    void setEnabled(bool enable) { enable = enable }
+    
+}
